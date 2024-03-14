77debdf2ef0b1ff2564378d050a2a9549ea38932
ameny <amenyelokb@gmail.com>
2024-03-14 11:57:07 +0100

    Quiz migration

---
+++ app/Models/Quiz.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Quiz extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['step_id', 'is_exam'];
+
+    public function step()
+    {
+        return $this->belongsTo(Step::class);
+    }
+
+    public function learningPath()
+    {
+        return $this->belongsTo(LearningPath::class);
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+}
---
+++ database/migrations/2024_02_11_093109_create_quizzes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            // nullable when the quiz is  an exam
+            $table->foreignId('step_id')->nullable()->constrained('steps')->onDelete('cascade');
+            // nullable when the quiz is not an exam
+            $table->foreignId('learning_path_id')->nullable()->constrained('learning_paths')->onDelete('cascade');
+            $table->boolean('is_exam')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
